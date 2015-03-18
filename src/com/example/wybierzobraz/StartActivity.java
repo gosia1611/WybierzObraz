a1394d57c7fa04da5ca09d7fad2a5284e2e5e553
gosia1611 <malgorzatka86@gmail.com>
2015-03-18 20:37:30 +0100

    WybierzObraz

---
+++ src/com/example/wybierzobraz/StartActivity.java
@@ -0,0 +1,49 @@
+package com.example.wybierzobraz;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class StartActivity extends Activity {
+	private int pageNumber = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        
+        final Button button = (Button) findViewById(R.id.button_ok);
+        final Context context = this;
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	performOk(context);
+            }
+        });
+    }
+    
+    private void performOk(Context context) {
+    	pageNumber++;
+    	switch (pageNumber) {
+    	case 2: 
+    		((TextView)findViewById(R.id.textView1)).setText(R.string.tekst2); break;
+    	case 3: 
+    		((TextView)findViewById(R.id.textView1)).setText(R.string.tekst3); break;
+    	case 4: 
+    		((TextView)findViewById(R.id.textView1)).setText(R.string.tekst4); break;
+    	case 5: 
+    		((TextView)findViewById(R.id.textView1)).setText(R.string.tekst5); break;
+    	case 6: 
+    		((TextView)findViewById(R.id.textView1)).setText(R.string.tekst6); break;
+    	case 8:
+    		Intent intent = new Intent(context, MainActivity.class);
+    		startActivity(intent);
+    		finish(); break;
+    }
+    }
+
+}
